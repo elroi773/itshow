01b7f4681a8006c8fd920989e34a43e4766c94ba
elroi773 <s2457@e-mirim.hs.kr>
2026-02-06 20:30:36 +0900

    onbording 2,3 구현중

---
+++ itshow/src/component/navigate.jsx
@@ -0,0 +1,34 @@
+import "./navigate.css";
+import { useNavigate } from "react-router-dom";
+
+export default function Navigate() {
+  const navigate = useNavigate();
+
+  const handleGoFirst = () => {
+    navigate("/onbording1");
+  };
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <footer className="join1-bottomNav">
+      <button
+        type="button"
+        className="join1-bottomLink"
+        onClick={handleGoFirst}
+      >
+        처음으로
+      </button>
+      <span className="join1-bottomDivider">|</span>
+      <button
+        type="button"
+        className="join1-bottomLink"
+        onClick={handleGoBack}
+      >
+        이전으로
+      </button>
+    </footer>
+  );
+}
