e210fbdd61fcd23162354208f12762f688d9c605
elroi773 <s2457@e-mirim.hs.kr>
2026-02-06 20:30:28 +0900

    onbording 2,3 구현중

--- itshow/src/App.jsx
+++ itshow/src/App.jsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./page/home";
-import OnBording from "./page/Join/onbording1";
-import OnBording2 from "./page/Join/onbording2";
+import OnBording from "./page/onbording/onbording1";
+import OnBording2 from "./page/onbording/onbording2";
+import OnBording3 from "./page/onbording/onbording3";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path="/home" element={<Home />} />
         <Route path="/onbording1" element={<OnBording />} />
         <Route path="/onbording2" element={<OnBording2 />} />
+        <Route path="/onbording3" element={<OnBording3 />} />
       </Routes>
     </BrowserRouter>
   );
